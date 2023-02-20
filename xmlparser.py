ac469c6f6d59c23d57342884f9498ad466df5798
Philip Lee <phil467@gmail.com>
2023-02-20 17:31:11 -0500

    Added commenting

--- xmlparser.py
+++ xmlparser.py
@@ -1,3 +1,15 @@
+"""
+XML Parser for Spritesheets
+
+Author: Philip Lee
+
+Take an XML file in the Starling format and writes a csv file.
+Attributes include: image name, x and y coordinates and image width and height
+
+Takes two command-line arguments: read file and write file
+Ex. python3 xmlparser.py readfile.xml writefile.csv
+"""
+
 import sys
 import csv
 import xml.etree.ElementTree as ET
@@ -12,10 +24,9 @@ for elem in root:
     line = elem.attrib
     elements.append(line)
 
-with open("sheet.csv", "w") as file:
+with open(sys.argv[2], "w") as file:
 
     for line in elements:
         writer = csv.DictWriter(file, fieldnames=['name', 'x', 'y', 'width', 'height'])
         writer.writerow({'name': line['name'], 'x': line['x'], 'y': line['y'], 
                         'width': line['width'], 'height': line['height']})
-
